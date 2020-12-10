52b2a376f7a76dfac07ae0ffb1e75e66761cb7ed
drew <andresalazar138@gmail.com>
2020-12-10 00:05:09 -0500

    made small tweaks to models in backend folder and added initial boilerplate for the frontend

---
+++ frontend/src/layouts/components/navbar/NavbarUser.js
@@ -0,0 +1,131 @@
+import React from "react"
+import {
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle,
+  Media,
+  Badge
+} from "reactstrap"
+import PerfectScrollbar from "react-perfect-scrollbar"
+import axios from "axios"
+import * as Icon from "react-feather"
+import classnames from "classnames"
+import ReactCountryFlag from "react-country-flag"
+import Autocomplete from "../../../components/@vuexy/autoComplete/AutoCompleteComponent"
+import { useAuth0 } from "../../../authServices/auth0/auth0Service"
+import { history } from "../../../history"
+import { IntlContext } from "../../../utility/context/Internationalization"
+
+const handleNavigation = (e, path) => {
+  e.preventDefault()
+  history.push(path)
+}
+
+const UserDropdown = props => {
+  const { logout, isAuthenticated } = useAuth0()
+  return (
+    <DropdownMenu right>
+      <DropdownItem
+        tag="a"
+        href="#"
+        onClick={e => handleNavigation(e, "/pages/profile")}
+      >
+        <Icon.User size={14} className="mr-50" />
+        <span className="align-middle">Edit Profile</span>
+      </DropdownItem>
+      <DropdownItem
+        tag="a"
+        href="#"
+        onClick={e => handleNavigation(e, "/email/inbox")}
+      >
+        <Icon.Mail size={14} className="mr-50" />
+        <span className="align-middle">My Inbox</span>
+      </DropdownItem>
+      <DropdownItem
+        tag="a"
+        href="#"
+        onClick={e => handleNavigation(e, "/todo/all")}
+      >
+        <Icon.CheckSquare size={14} className="mr-50" />
+        <span className="align-middle">Tasks</span>
+      </DropdownItem>
+      <DropdownItem
+        tag="a"
+        href="#"
+        onClick={e => handleNavigation(e, "/chat")}
+      >
+        <Icon.MessageSquare size={14} className="mr-50" />
+        <span className="align-middle">Chats</span>
+      </DropdownItem>
+      <DropdownItem tag="a" href="#" onClick={e => handleNavigation(e, "/ecommerce/wishlist")}>
+        <Icon.Heart size={14} className="mr-50" />
+        <span className="align-middle">WishList</span>
+      </DropdownItem>
+      <DropdownItem divider />
+      <DropdownItem
+        tag="a"
+        href="/pages/login"
+        onClick={e => {
+          e.preventDefault()
+          if (isAuthenticated) {
+            return logout({
+              returnTo: window.location.origin + process.env.REACT_APP_PUBLIC_PATH
+            })
+          } else {
+            const provider = props.loggedInWith
+            if (provider !== null) {
+              if (provider === "jwt") {
+                return props.logoutWithJWT()
+              }
+              if (provider === "firebase") {
+                return props.logoutWithFirebase()
+              }
+            } else {
+              history.push("/pages/login")
+            }
+          }
+
+        }}
+      >
+        <Icon.Power size={14} className="mr-50" />
+        <span className="align-middle">Log Out</span>
+      </DropdownItem>
+    </DropdownMenu>
+  )
+}
+
+class NavbarUser extends React.PureComponent {
+
+  render() {
+
+    return (
+      <ul className="nav navbar-nav navbar-nav-user float-right">
+        <UncontrolledDropdown tag="li" className="dropdown-user nav-item">
+          <DropdownToggle tag="a" className="nav-link dropdown-user-link">
+            <div className="user-nav d-sm-flex d-none">
+              <span className="user-name text-bold-600">
+                {this.props.userName}
+              </span>
+              <span className="user-status">Available</span>
+            </div>
+            <span data-tour="user">
+              <img
+                src={this.props.userImg}
+                className="round"
+                height="40"
+                width="40"
+                alt="avatar"
+              />
+            </span>
+          </DropdownToggle>
+          <UserDropdown {...this.props} />
+        </UncontrolledDropdown>
+      </ul>
+    )
+  }
+}
+export default NavbarUser
