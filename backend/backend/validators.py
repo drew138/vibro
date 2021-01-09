46ddd513f17fa7dcc88fcca0a3687828cf2e85a9
drew <andresalazar138@gmail.com>
2021-01-09 13:18:43 -0500

    added regex validators for phone numbers and and celphone numbers

---
+++ backend/backend/validators.py
@@ -0,0 +1,26 @@
+from django.core.validators import RegexValidator
+
+PHONE_REGEX = r"^(((\(\+?\d{2,3}\))|(\+?\d{2,3}))[\s\-])?\d{3}[\s\-]\d{4}([\s\-]ext[\s\-]\d{1,3})?$"
+
+CELPHONE_REGEX = r"^((\(\+?\d{2,3}\))|(\+?\d{2,3})[\s\-])?\d{3}([\-\s]\d{4}){2}$"
+
+PHONE_MESSAGE = \
+    "El número de teléfono debe " \
+    "ser entrado en el formato: " \
+    "(+xxx) xxx xxxx ext xxx " \
+    "siendo el código de área y " \
+    "la extensión opcionales."
+
+CELPHONE_MESSAGE = \
+    "El número de celular debe " \
+    "ser entrado en el formato: " \
+    "(+xxx) xxx xxxx xxxx siendo " \
+    "el código de país opcional"
+
+PHONE_REGEX_VALIDATOR = RegexValidator(
+    regex=PHONE_REGEX,
+    message=PHONE_MESSAGE)
+
+CELPHONE_REGEX_VALIDATOR = RegexValidator(
+    regex=CELPHONE_REGEX,
+    message=CELPHONE_MESSAGE)
